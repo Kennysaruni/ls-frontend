908892bce937d4df34a1846c72d5daeef85e1be2
Kennysaruni <kenny.saruni@student.moringaschool.com>
2024-02-29 00:38:12 +0300

    Added links page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Links from './components/Links';
 import Form from './components/Form';
 import Navbar from './components/Navbar';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <Navbar/>
+      {/* <Links/> */}
       <Form/>
     </div>
   );
---
+++ src/components/Links.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import './Links.css'
+import API_BASE_URL from '../utilities/env'
+
+function Links() {
+    const [data,setData] = useState([])
+    const items = 5
+    const [next,setNext] = useState(items)
+
+    useEffect(() => {
+        fetch(`${API_BASE_URL}/s`)
+        .then(res => res.json())
+        .then(data => setData(data.links))
+    },[])
+
+    function loadMore(){
+        setNext(next + items )
+    }
+
+  return (
+    <div className='container'>
+        <h1 className='title'>
+            Your Links
+        </h1>
+        <label htmlFor="filter"> Filter links
+            <input type="text" placeholder='Search for links' id='filter' className='input-block' />
+        </label>
+        <div className="table">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Short link</th>
+                        <th>Original Url</th>
+                        <th>Clicks</th>
+                        <th>Key</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.slice(0,next).map((link) => (
+                        (
+                            <tr key={link.id}>
+                                <td>{`${API_BASE_URL}/${link.slug}`}</td>
+                                <td className='original-url'>{link.url}</td>
+                                <td>{link.clicked}</td>
+                                <td>{link.slug}</td>
+                                <td>
+                                    <buttons className='edit'>Edit</buttons>
+                                    <buttons className='delete' >Delete</buttons>
+                                </td>
+                            </tr>
+                        )
+                    ))}
+                </tbody>
+            </table>
+        </div>
+        {next < data.length && (
+                <button className="load-more" onClick={loadMore}>Load more</button>
+            )}
+    </div>
+  )
+}
+
+export default Links
